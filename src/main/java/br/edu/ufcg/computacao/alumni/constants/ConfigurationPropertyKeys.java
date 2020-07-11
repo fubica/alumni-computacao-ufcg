1416b82d98ff07d5e0886af17951a477dbc23300
fubica <fubica@dsc.ufcg.edu.br>
2020-07-11 16:47:53 -0300

    Deleted old code

--- src/main/java/br/edu/ufcg/computacao/alumni/constants/ConfigurationPropertyKeys.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ufcg.computacao.alumni.constants;
-
-public class ConfigurationPropertyKeys {
-    public static final String MEMBERS_LIST_KEY = "members_list";
-    public static final String CLIENT_ID = "client_id";
-    public static final String CLIENT_SECRET = "client_secret";
-    public static final String CALLBACK_URI = "callback_uri";
-}
